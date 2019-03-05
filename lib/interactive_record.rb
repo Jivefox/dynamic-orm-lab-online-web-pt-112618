3cad0c3c546f76fe2576f258b77774decdd08c35
Cliff Jurgensen <jivefox.002@gmail.com>
2019-03-05 04:20:42 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -39,7 +39,7 @@ class InteractiveRecord
     self.class.colunm_names.each do |col_name|
       values << "'#{send(col_name)}'" unless send(col_name).nil?
     end
-    values.join(, )
+    values.join(", ")
   end
 
 end
